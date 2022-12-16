1e45df0cfd43d7218c832235da7ef7a8e5ad3829
Yuri-Punzo <yuripunzo@hotmail.it>
2022-12-16 17:21:08 +0100

    added comment with consegna

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,25 @@
+<!-- 
+CONSEGNA:
+Create un nuovo progetto Laravel.
+composer create-project laravel/laravel laravel-comics
+cd laravel-comics
+composer require pacificdev/laravel_9_preset
+php artisan preset:ui bootstrap
+
+// il seguente comando blocca il terminale (funziona solo su bash e non su powershell)
+npm i && npm run dev
+
+// apri un nuovo terminale
+php artisan serve
+Nota: per uscire dal terminale quando é bloccato dai processi npm run dev o php artisan serve   premete ctrl+c o ctrl+z
+Concentratevi sul layout:
+create un file di layout in cui inserire la struttura comune di tutte le pagine del sito web (tag head, tag body, ...)
+ed includendo header e footer tramite due partials.
+Create poi una rotta per visualizzare la lista di tutti i fumetti recuperati da un file inserito nella cartella config e abbellite il tutto sfruttando Sass.
+Bonus:
+Create più pagine che estendono lo stesso layout
+ -->
+
 @extends("layouts.app")
 
 @section("content")
