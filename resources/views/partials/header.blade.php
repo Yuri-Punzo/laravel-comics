87370143074cdeff5a85991fc7d61a1e1148b8ac
Yuri-Punzo <yuripunzo@hotmail.it>
2022-12-16 16:48:15 +0100

    wip

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <div id="site_header">
+        <div class="container">
+            <div class="row">
+                <div class="col-auto">
+                    <div class="logo">
+                        <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="">
+                    </div>
+                </div>
+                <div class="col">
+                    <nav class="my_navbar">
+                        <a href="#">
+                        </a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="jumbotron">
+
+        </div>
+
+    </div>
+</header>
